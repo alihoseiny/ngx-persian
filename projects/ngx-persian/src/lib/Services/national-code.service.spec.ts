f18c4b0d6e7018dc942ea82666d3205f0936c817
alihoseiny <barzi1375@gmail.com>
2019-04-13 23:02:02 +0430

    Add isValid method to the NationalCodeService ============================== - Add isValid method to the NationalCodeService for checking is input national code a valid one or not.

--- projects/ngx-persian/src/lib/Services/national-code.service.spec.ts
+++ projects/ngx-persian/src/lib/Services/national-code.service.spec.ts
@@ -57,5 +57,67 @@ describe('NationalCodeService', () => {
 
   });
 
+  describe('isValid', () => {
+    it ('Should return false when input is undefined', () => {
+        expect(nationalCodeService.isValid(undefined)).toBeFalsy();
+    });
+
+    it ('Should return false when input is null', () => {
+        expect(nationalCodeService.isValid(null)).toBeFalsy();
+    });
+
+    it ('Should return false when input is an empty string', () => {
+        expect(nationalCodeService.isValid('')).toBeFalsy();
+    });
+  });
+
+  [
+    1111111111,
+    333333333,
+    44444444,
+    '9999999999',
+    '000000000',
+    '66666666'
+  ].forEach(input => {
+    it(`should return false when all digits of the input (${input}) are equal.`, () => {
+      expect(nationalCodeService.isValid(input)).toBeFalsy();
+    });
+  });
+
+  [
+    '00234178913',
+    1364756,
+    1,
+    2345678924545445454
+  ].forEach(input => {
+    it(`should return false when input (${input}) length is not in the valid range.`, () => {
+      expect(nationalCodeService.isValid(input)).toBeFalsy();
+    });
+  });
+
+  [
+    '0023417891',
+    12364756,
+    1234567892,
+    234567892,
+    7731689951
+  ].forEach(input => {
+    it(`should return false when input value (${input}) is not a valid national code according to the validation algorithm.`, () => {
+      expect(nationalCodeService.isValid(input)).toBeFalsy();
+    });
+  });
+
+  [
+    1234567891,
+    '0016245784',
+    '236547887',
+    2365478824,
+    654127859,
+    '16245784'
+  ].forEach(input => {
+    it(`should return true when input (${input}) is a valid national code.`, () => {
+      expect(nationalCodeService.isValid(input)).toBeTruthy();
+    });
+  })
 
 });
