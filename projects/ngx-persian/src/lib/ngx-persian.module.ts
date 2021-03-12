57f61f1d85181f52fb3c5a0d633281b8d541f0cd
Mohammadreza Alihoseiny <barzi1375@gmail.com>
2021-03-12 20:11:00 +0330

    Remove redundant ExperimentalPipePipe import

--- projects/ngx-persian/src/lib/ngx-persian.module.ts
+++ projects/ngx-persian/src/lib/ngx-persian.module.ts
@@ -5,9 +5,8 @@ import {EnNumPipe} from './Pipes/enNum.pipe';
 import {IRCurrencyPipe} from './Pipes/IRCurrency.pipe';
 import {NationalCodePipe} from './Pipes/nationalCode.pipe';
 import {JdatePipe} from './Pipes/jdate.pipe';
-import { ExperimentalPipePipe } from './Pipes/experimental-pipe.pipe';
 @NgModule({
-  declarations: [PersianLetterDirective, PersianNumbersDirective, FaNumPipe, EnNumPipe, IRCurrencyPipe, NationalCodePipe, JdatePipe, ExperimentalPipePipe],
+  declarations: [PersianLetterDirective, PersianNumbersDirective, FaNumPipe, EnNumPipe, IRCurrencyPipe, NationalCodePipe, JdatePipe],
   imports: [],
   exports: [PersianLetterDirective, PersianNumbersDirective, FaNumPipe, EnNumPipe, IRCurrencyPipe, NationalCodePipe, JdatePipe]
 })
